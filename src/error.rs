4ec2f88662114d08a31fc71494a26118f3dec324
Michael Ma <michael60612@gmail.com>
2016-04-14 19:35:49 -0700

    implement Error trait for StatsError

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,39 @@
+use std::error::Error;
+use std::fmt;
+
 /// Enumeration of possible errors thrown
 /// within the statrs library
-#[derive(Debug)]
 pub enum StatsError {
     BadParams,
     ArgMustBePositive(&'static str),
     ArgNotNegative(&'static str),
     ArgIntervalIncl(&'static str, f64, f64),
+}
+
+impl Error for StatsError {
+    fn description(&self) -> &str {
+        "Error performing statistical calculation"
+    }
+}
+
+impl fmt::Display for StatsError{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            StatsError::BadParams => write!(f, "Bad distribution parameters"),
+            StatsError::ArgMustBePositive(s) => write!(f, "Argument {} must be positive", s),
+            StatsError::ArgNotNegative(s) => write!(f, "Argument {} must be non-negative", s),
+            StatsError::ArgIntervalIncl(s, min, max) => write!(f, "Argument {} not within interval [{}, {}]", s, min, max),
+        }
+    }
+}
+
+impl fmt::Debug for StatsError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            StatsError::BadParams => write!(f, "Bad distribution parameters"),
+            StatsError::ArgMustBePositive(s) => write!(f, "Argument {} must be positive", s),
+            StatsError::ArgNotNegative(s) => write!(f, "Argument {} must be non-negative", s),
+            StatsError::ArgIntervalIncl(s, min, max) => write!(f, "Argument {} not within interval [{}, {}]", s, min, max),
+        }
+    }
 }
