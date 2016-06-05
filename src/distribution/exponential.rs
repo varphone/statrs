1a89f586015f4d5488ce709aaf1c49678d2ba757
Michael Ma <michael60612@gmail.com>
2016-06-05 13:46:44 -0700

    begin tests for exponential distribution

--- src/distribution/exponential.rs
+++ src/distribution/exponential.rs
@@ -92,3 +92,118 @@ impl Continuous for Exponential {
         self.rate.ln() - self.rate * x
     }
 }
+
+#[cfg_attr(rustfmt, rustfmt_skip)]
+#[cfg(test)]
+mod test {
+    use std::f64;
+    use distribution::{Univariate, Continuous};
+    use prec;
+    use super::Exponential;
+    
+    fn try_create(rate: f64) -> Exponential {
+        let n = Exponential::new(rate);
+        assert!(n.is_ok());
+        n.unwrap()
+    }
+    
+    fn create_case(rate: f64) {
+        let n = try_create(rate);
+        assert_eq!(rate, n.rate());
+    }
+    
+    fn bad_create_case(rate: f64) {
+        let n = Exponential::new(rate);
+        assert!(n.is_err());
+    }
+
+    fn test_case<F>(rate: f64, expected: f64, eval: F)
+        where F: Fn(Exponential) -> f64
+    {
+        let n = try_create(rate);
+        let x = eval(n);
+        assert_eq!(expected, x);
+    }
+
+    fn test_almost<F>(rate: f64, expected: f64, acc: f64, eval: F)
+        where F: Fn(Exponential) -> f64
+    {
+        let n = try_create(rate);
+        let x = eval(n);
+        assert!(prec::almost_eq(expected, x, acc));
+    }
+    
+    #[test]
+    fn test_create() {
+        create_case(0.1);
+        create_case(1.0);
+        create_case(10.0);
+    }
+    
+    #[test]
+    fn test_bad_create() {
+        bad_create_case(f64::NAN);
+        bad_create_case(0.0);
+        bad_create_case(-1.0);
+        bad_create_case(-10.0);
+    }
+    
+    #[test]
+    fn test_mean() {
+        test_case(0.1, 10.0, |x| x.mean());
+        test_case(1.0, 1.0, |x| x.mean());
+        test_case(10.0, 0.1, |x| x.mean());
+    }
+    
+    #[test]
+    fn test_variance() {
+        test_almost(0.1, 100.0, 1e-13, |x| x.variance());
+        test_case(1.0, 1.0, |x| x.variance());
+        test_case(10.0, 0.01, |x| x.variance());
+    }
+    
+    #[test]
+    fn test_std_dev() {
+        test_case(0.1, 10.0, |x| x.std_dev());
+        test_case(1.0, 1.0, |x| x.std_dev());
+        test_case(10.0, 0.1, |x| x.std_dev());
+    }
+    
+    #[test]
+    fn test_entropy() {
+        test_almost(0.1, 3.302585092994045684018, 1e-15, |x| x.entropy());
+        test_case(1.0, 1.0, |x| x.entropy());
+        test_almost(10.0, -1.302585092994045684018, 1e-15, |x| x.entropy());
+    }
+    
+    #[test]
+    fn test_skewness() {
+        test_case(0.1, 2.0, |x| x.skewness());
+        test_case(1.0, 2.0, |x| x.skewness());
+        test_case(10.0, 2.0, |x| x.skewness());
+    }
+    
+    #[test]
+    fn test_median() {
+        test_almost(0.1, 6.931471805599453094172, 1e-15, |x| x.median());
+        test_case(1.0, f64::consts::LN_2, |x| x.median());
+        test_case(10.0, 0.06931471805599453094172, |x| x.median());
+    }
+    
+    #[test]
+    fn test_mode() {
+        test_case(0.1, 0.0, |x| x.mode());
+        test_case(1.0, 0.0, |x| x.mode());
+        test_case(10.0, 0.0, |x| x.mode());
+    }
+    
+    #[test]
+    fn test_min_max() {
+        test_case(0.1, 0.0, |x| x.min());
+        test_case(1.0, 0.0, |x| x.min());
+        test_case(10.0, 0.0, |x| x.min());
+        test_case(0.1, f64::INFINITY, |x| x.max());
+        test_case(1.0, f64::INFINITY, |x| x.max());
+        test_case(10.0, f64::INFINITY, |x| x.max());
+    }
+}
