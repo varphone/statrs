3f4873dad000766c30027d1ae116ea772bb494fc
Michael Ma <michael60612@gmail.com>
2016-06-09 18:10:53 -0700

    complete tests for exponential distribution

--- src/distribution/exponential.rs
+++ src/distribution/exponential.rs
@@ -234,4 +234,44 @@ mod test {
         test_case(10.0, 0.0, |x| x.pdf(f64::INFINITY));
         test_is_nan(f64::INFINITY, |x| x.pdf(f64::INFINITY));
     }
+    
+    #[test]
+    fn test_ln_pdf() {
+        test_almost(0.1, -2.302585092994045684018, 1e-15, |x| x.ln_pdf(0.0));
+        test_case(1.0, 0.0, |x| x.ln_pdf(0.0));
+        test_case(10.0, 2.302585092994045684018, |x| x.ln_pdf(0.0));
+        test_is_nan(f64::INFINITY, |x| x.ln_pdf(0.0));
+        test_almost(0.1, -2.312585092994045684018, 1e-15, |x| x.ln_pdf(0.1));
+        test_case(1.0, -0.1, |x| x.ln_pdf(0.1));
+        test_almost(10.0, 1.302585092994045684018, 1e-15, |x| x.ln_pdf(0.1));
+        test_is_nan(f64::INFINITY, |x| x.ln_pdf(0.1));
+        test_case(0.1, -2.402585092994045684018, |x| x.ln_pdf(1.0));
+        test_case(1.0, -1.0, |x| x.ln_pdf(1.0));
+        test_case(10.0, -7.697414907005954315982, |x| x.ln_pdf(1.0));
+        test_is_nan(f64::INFINITY, |x| x.ln_pdf(1.0));
+        test_case(0.1, f64::NEG_INFINITY, |x| x.ln_pdf(f64::INFINITY));
+        test_case(1.0, f64::NEG_INFINITY, |x| x.ln_pdf(f64::INFINITY));
+        test_case(10.0, f64::NEG_INFINITY, |x| x.ln_pdf(f64::INFINITY));
+        test_is_nan(f64::INFINITY, |x| x.ln_pdf(f64::INFINITY));
+    }
+    
+    #[test]
+    fn test_cdf() {
+        test_case(0.1, 0.0, |x| x.cdf(0.0));
+        test_case(1.0, 0.0, |x| x.cdf(0.0));
+        test_case(10.0, 0.0, |x| x.cdf(0.0));
+        test_is_nan(f64::INFINITY, |x| x.cdf(0.0));
+        test_almost(0.1, 0.009950166250831946426094, 1e-16, |x| x.cdf(0.1));
+        test_almost(1.0, 0.0951625819640404268358, 1e-16, |x| x.cdf(0.1));
+        test_case(10.0, 0.6321205588285576784045, |x| x.cdf(0.1));
+        test_case(f64::INFINITY, 1.0, |x| x.cdf(0.1));
+        test_almost(0.1, 0.0951625819640404268358, 1e-16, |x| x.cdf(1.0));
+        test_case(1.0, 0.6321205588285576784045, |x| x.cdf(1.0));
+        test_case(10.0, 0.9999546000702375151485, |x| x.cdf(1.0));
+        test_case(f64::INFINITY, 1.0, |x| x.cdf(1.0));
+        test_case(0.1, 1.0, |x| x.cdf(f64::INFINITY));
+        test_case(1.0, 1.0, |x| x.cdf(f64::INFINITY));
+        test_case(10.0, 1.0, |x| x.cdf(f64::INFINITY));
+        test_case(f64::INFINITY, 1.0, |x| x.cdf(f64::INFINITY));
+    }
 }
