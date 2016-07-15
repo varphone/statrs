f70201a2a6374c68173067445e31843144b12395
Michael Ma <michael60612@gmail.com>
2016-07-14 20:34:36 -0700

    begin documenting bernoulli, implement travis yaml file

--- src/distribution/bernoulli.rs
+++ src/distribution/bernoulli.rs
@@ -3,20 +3,76 @@ use rand::distributions::{Sample, IndependentSample};
 use result::Result;
 use super::{Binomial, Distribution, Univariate, Discrete};
 
+/// Implements the [Bernoulli](https://en.wikipedia.org/wiki/Bernoulli_distribution)
+/// distribution which is a special case of the [Binomial](https://en.wikipedia.org/wiki/Binomial_distribution)
+/// distribution where `n = 1`
+///
+/// # Examples
+///
+/// ```
+/// use statrs::distribution::{Bernoulli, Univariate, Discrete};
+///
+/// let n = Bernoulli::new(0.5).unwrap(); 
+/// assert_eq!(n.mean(), 0.5);
+/// assert_eq!(n.pmf(0), 0.5);
+/// assert_eq!(n.pmf(1), 0.5);
+/// assert_eq!(n.pmf(2), 0.0);
+/// ```
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Bernoulli {
     b: Binomial,
 }
 
 impl Bernoulli {
+    /// Constructs a new Bernoulli distribution with
+    /// the given `p` probability of success.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if `p` is `NaN, less than `0`
+    /// or greater than `1`
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use statrs::distribution::Bernoulli;
+    ///
+    /// let mut result = Bernoulli::new(0.5);
+    /// assert!(result.is_ok());
+    ///
+    /// result = Bernoulli::new(-0.5);
+    /// assert!(result.is_err());
+    /// ```
     pub fn new(p: f64) -> Result<Bernoulli> {
         Binomial::new(p, 1).map(|b| Bernoulli { b: b })
     }
 
+    /// Returns the probability of success `p` of the
+    /// Bernoulli distribution.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use statrs::distribution::Bernoulli;
+    ///
+    /// let n = Bernoulli::new(0.5).unwrap();
+    /// assert_eq!(n.p(), 0.5);
+    /// ```
     pub fn p(&self) -> f64 {
         self.b.p()
     }
 
+    /// Returns the number of trials `n` of the
+    /// Bernoulli distribution. Will always be `1.0`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use statrs::distribution::Bernoulli;
+    ///
+    /// let n = Bernoulli::new(0.5).unwrap();
+    /// assert_eq!(n.n(), 1.0);
+    /// ```
     pub fn n(&self) -> f64 {
         1.0
     }
