739d7f7c47b5b82a6e6164f44ddd5861ed7c6b2e
Michael Ma <michael60612@gmail.com>
2016-09-13 20:36:54 -0400

    docs for generate

--- src/generate.rs
+++ src/generate.rs
@@ -2,10 +2,31 @@
 use std::f64::consts;
 use euclid::Modulus;
 
+/// Creates a vector of `f64` points representing a periodic wave with an amplitude
+/// of `1.0`, phase of `0.0`, and delay of `0`.
+///
+/// # Examples
+///
+/// ```
+/// use statrs::generate;
+///
+/// let x = generate::periodic(10, 8.0, 2.0);
+/// assert_eq!(x, [0.0, 0.25, 0.5, 0.75, 0.0, 0.25, 0.5, 0.75, 0.0, 0.25]);
+/// ```
 pub fn periodic(length: usize, sampling_rate: f64, frequency: f64) -> Vec<f64> {
     periodic_custom(length, sampling_rate, frequency, 1.0, 0.0, 0)
 }
 
+/// Creates a vector of `f64` points representing a periodic wave.
+///
+/// # Examples
+///
+/// ```
+/// use statrs::generate;
+///
+/// let x = generate::periodic_custom(10, 8.0, 2.0, 10.0, 1.0, 2);
+/// assert_eq!(x, [6.0, 8.5, 1.0, 3.5, 6.0, 8.5, 1.0, 3.5, 6.0, 8.5]);
+/// ```
 pub fn periodic_custom(length: usize,
                        sampling_rate: f64,
                        frequency: f64,
@@ -32,10 +53,39 @@ pub fn periodic_custom(length: usize,
     data
 }
 
+/// Creates a vector of `f64` points representing a Sine wave with a mean of `0.0`,
+/// phase of `0.0`, and delay of `0`.
+///
+/// # Examples
+///
+/// ```
+/// use statrs::generate;
+///
+/// let x = generate::sinusoidal(10, 8.0, 2.0, 1.0);
+/// assert_eq!(x, 
+///     [0.0, 1.0, 0.00000000000000012246467991473532, 
+///     -1.0, -0.00000000000000024492935982947064, 1.0, 
+///     0.00000000000000036739403974420594, -1.0, 
+///     -0.0000000000000004898587196589413, 1.0]);
+/// ```
 pub fn sinusoidal(length: usize, sampling_rate: f64, frequency: f64, amplitude: f64) -> Vec<f64> {
     sinusoidal_custom(length, sampling_rate, frequency, amplitude, 0.0, 0.0, 0)
 }
 
+/// Creates a vector of `f64` points representing a Sine wave.
+///
+/// # Examples
+///
+/// ```
+/// use statrs::generate;
+///
+/// let x = generate::sinusoidal_custom(10, 8.0, 2.0, 1.0, 5.0, 2.0, 1);
+/// assert_eq!(x, 
+///     [5.416146836547142, 5.909297426825682, 4.583853163452858, 
+///     4.090702573174318, 5.416146836547142, 5.909297426825682, 
+///     4.583853163452858, 4.090702573174318, 5.416146836547142, 
+///     5.909297426825682]);
+/// ```
 pub fn sinusoidal_custom(length: usize,
                          sampling_rate: f64,
                          frequency: f64,
