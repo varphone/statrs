b848cd48543b4479af5b30d79a126840abfe8165
Michael Ma <michael60612@gmail.com>
2016-10-10 22:52:43 -0400

    made iter stats abs_min more idiomatic

--- src/statistics/iter_statistics.rs
+++ src/statistics/iter_statistics.rs
@@ -32,17 +32,12 @@ impl<T: Iterator<Item=f64>> IterStatistics<f64> for T {
     fn abs_min(&mut self) -> f64 {
         let mut min = f64::INFINITY;
         let mut any = false;
-        loop {
-            match self.next() {
-                Some(x) => {
-                    let abs = x.abs();
-                    if abs < min || abs.is_nan() {
-                        min = abs;
-                    }
-                    any = true;
-                }
-                None => break
+        for x in self {
+            let abs = x.abs();
+            if abs < min || abs.is_nan() {
+                min = abs;
             }
+            any = true;
         }
         if any { min } else { f64::NAN }
     }
