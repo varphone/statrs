0a0f228359942aa8e47f588d25813751c1c68489
Michael Ma <michael60612@gmail.com>
2016-10-10 23:04:02 -0400

    implement abs_max for IterStats

--- src/statistics/iter_statistics.rs
+++ src/statistics/iter_statistics.rs
@@ -1,9 +1,8 @@
 use std::f64;
 
 /// The `IterStatistics` trait provides the same host of statistical
-/// utilities as the `Statistics` traited ported for use with iterators 
-pub trait IterStatistics<T> : Iterator<Item=T>
-{
+/// utilities as the `Statistics` traited ported for use with iterators
+pub trait IterStatistics<T>: Iterator<Item = T> {
     /// Returns the minimum absolute value in the data
     ///
     /// # Remarks
@@ -16,29 +15,61 @@ pub trait IterStatistics<T> : Iterator<Item=T>
     /// use std::f64;
     /// use statrs::statistics::IterStatistics;
     ///
-    /// let x: Vec<f64> = vec![];
+    /// let x = vec![];
     /// assert!(x.into_iter().abs_min().is_nan());
     ///
-    /// let y: Vec<f64>  = vec![0.0, f64::NAN, 3.0, -2.0];
+    /// let y = vec![0.0, f64::NAN, 3.0, -2.0];
     /// assert!(y.into_iter().abs_min().is_nan());
     ///
-    /// let z: Vec<f64>  = vec![0.0, 3.0, -2.0];
+    /// let z = vec![0.0, 3.0, -2.0];
     /// assert_eq!(z.into_iter().abs_min(), 0.0);
     /// ```
     fn abs_min(&mut self) -> T;
+
+    /// Returns the maximum absolute value in the data
+    ///
+    /// # Remarks
+    ///
+    /// Returns `f64::NAN` if data is empty or an entry is `f64::NAN`
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use std::f64;
+    /// use statrs::statistics::IterStatistics;
+    ///
+    /// let x = vec![];
+    /// assert!(x.into_iter().abs_max().is_nan());
+    ///
+    /// let y = vec![0.0, f64::NAN, 3.0, -2.0];
+    /// assert!(y.into_iter().abs_max().is_nan());
+    ///
+    /// let z = vec![0.0, 3.0, -2.0, -8.0];
+    /// assert_eq!(z.into_iter().abs_max(), 8.0);
+    /// ```
+    fn abs_max(&mut self) -> T;
 }
 
-impl<T: Iterator<Item=f64>> IterStatistics<f64> for T {
+impl<T: Iterator<Item = f64>> IterStatistics<f64> for T {
     fn abs_min(&mut self) -> f64 {
-        let mut min = f64::INFINITY;
-        let mut any = false;
-        for x in self {
-            let abs = x.abs();
-            if abs < min || abs.is_nan() {
-                min = abs;
+        match self.next() {
+            None => f64::NAN,
+            Some(x) => {
+                self.map(|x| x.abs())
+                    .fold(x.abs(),
+                          |acc, x| if x < acc || x.is_nan() { x } else { acc })
             }
-            any = true;
         }
-        if any { min } else { f64::NAN }
+    }
+
+    fn abs_max(&mut self) -> f64 {
+        match self.next() {
+            None => f64::NAN,
+            Some(x) => {
+                self.map(|x| x.abs())
+                    .fold(x.abs(),
+                          |acc, x| if x > acc || x.is_nan() { x } else { acc })
+            }
+        }
     }
 }
