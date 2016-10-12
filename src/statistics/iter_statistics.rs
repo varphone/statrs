fc2ce127fd79a77c5b9630e18832340fd24c6703
Michael Ma <michael60612@gmail.com>
2016-10-11 22:21:39 -0400

    begin support for iterators with &f64 vs f64

--- src/statistics/iter_statistics.rs
+++ src/statistics/iter_statistics.rs
@@ -2,7 +2,8 @@ use std::f64;
 
 /// The `IterStatistics` trait provides the same host of statistical
 /// utilities as the `Statistics` traited ported for use with iterators
-pub trait IterStatistics<T>: Iterator<Item = T> {
+/// which requires a mutable borrow
+pub trait IterStatistics<I, T> : Iterator<Item=I> {
     /// Returns the minimum absolute value in the data
     ///
     /// # Remarks
@@ -15,14 +16,14 @@ pub trait IterStatistics<T>: Iterator<Item = T> {
     /// use std::f64;
     /// use statrs::statistics::IterStatistics;
     ///
-    /// let x = vec![];
-    /// assert!(x.into_iter().abs_min().is_nan());
+    /// let x: Vec<f64> = vec![];
+    /// assert!(x.iter().abs_min().is_nan());
     ///
     /// let y = vec![0.0, f64::NAN, 3.0, -2.0];
-    /// assert!(y.into_iter().abs_min().is_nan());
+    /// assert!(y.iter().abs_min().is_nan());
     ///
     /// let z = vec![0.0, 3.0, -2.0];
-    /// assert_eq!(z.into_iter().abs_min(), 0.0);
+    /// assert_eq!(z.iter().abs_min(), 0.0);
     /// ```
     fn abs_min(&mut self) -> T;
 
@@ -38,7 +39,7 @@ pub trait IterStatistics<T>: Iterator<Item = T> {
     /// use std::f64;
     /// use statrs::statistics::IterStatistics;
     ///
-    /// let x = vec![];
+    /// let x: Vec<f64> = vec![];
     /// assert!(x.into_iter().abs_max().is_nan());
     ///
     /// let y = vec![0.0, f64::NAN, 3.0, -2.0];
@@ -50,7 +51,32 @@ pub trait IterStatistics<T>: Iterator<Item = T> {
     fn abs_max(&mut self) -> T;
 }
 
-impl<T: Iterator<Item = f64>> IterStatistics<f64> for T {
+impl<'a, T: Iterator<&'a f64>> IterStatistics<&'a f64, f64> for T 
+{
+    fn abs_min(&mut self) -> f64 {
+        match self.next() {
+            None => f64::NAN,
+            Some(x) => {
+                self.map(|x| x.abs())
+                    .fold(x.abs(),
+                          |acc, x| if x < acc || x.is_nan() { x } else { acc })
+            }
+        }
+    }
+
+    fn abs_max(&mut self) -> f64 {
+        match self.next() {
+            None => f64::NAN,
+            Some(x) => {
+                self.map(|x| x.abs())
+                    .fold(x.abs(),
+                          |acc, x| if x > acc || x.is_nan() { x } else { acc })
+            }
+        }
+    }
+}
+
+impl<'a, T: Iterator<f64>> IterStatistics<f64, f64> for T {
     fn abs_min(&mut self) -> f64 {
         match self.next() {
             None => f64::NAN,
