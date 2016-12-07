3144766d83f4478a7677c2e843ec4ebeed15fc39
Michael Ma <michael60612@gmail.com>
2016-12-06 20:34:07 -0500

    feat: impl and tested exponential integral

--- src/function/exponential.rs
+++ src/function/exponential.rs
@@ -31,7 +31,7 @@ use consts;
 ///
 /// ```
 /// ```
-fn integral(x: f64, n: u64) -> Result<f64> {
+pub fn integral(x: f64, n: u64) -> Result<f64> {
     let eps = 0.00000000000000001;
     let max_iter = 100;
     let nf64 = n as f64;
@@ -51,7 +51,7 @@ fn integral(x: f64, n: u64) -> Result<f64> {
         let mut d = 1.0 / b;
         let mut h = d;
         for i in 1..max_iter + 1 {
-            let a = -1.0 * i as f64 * (nf64 - 1.0) + i as f64;
+            let a = -1.0 * i as f64 * (nf64 - 1.0 + i as f64);
             b += 2.0;
             d = 1.0 / (a * d + b);
             c = b + a / c;
@@ -64,18 +64,18 @@ fn integral(x: f64, n: u64) -> Result<f64> {
         Err(StatsError::ComputationFailedToConverge)
     } else {
         let mut factorial = 1.0;
-        let mut result = if nf64 - 1.0 != 0.0 {
+        let mut result = if n - 1 != 0 {
             1.0 / (nf64 - 1.0)
         } else {
             -1.0 * x.ln() - consts::EULER_MASCHERONI
         };
         for i in 1..max_iter + 1 {
             factorial *= -1.0 * x / i as f64;
-            let del = if i as f64 != nf64 - 1.0 {
+            let del = if i != n - 1 {
                 -factorial / (i as f64 - nf64 + 1.0)
             } else {
                 let mut psi = -1.0 * consts::EULER_MASCHERONI;
-                for ii in 1..(nf64 as usize) {
+                for ii in 1..n {
                     psi += 1.0 / ii as f64;
                 }
                 factorial * (-1.0 * x.ln() + psi)
@@ -86,5 +86,29 @@ fn integral(x: f64, n: u64) -> Result<f64> {
             }
         }
         Err(StatsError::ComputationFailedToConverge)
+    }
+}
+
+#[cfg_attr(rustfmt, rustfmt_skip)]
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_integral() {
+        assert_eq!(super::integral(0.001, 1).unwrap(), 6.33153936413614904);
+        assert_almost_eq!(super::integral(0.1, 1).unwrap(), 1.82292395841939059, 1e-15);
+        assert_eq!(super::integral(1.0, 1).unwrap(), 0.219383934395520286);
+        assert_almost_eq!(super::integral(2.0, 1).unwrap(), 0.0489005107080611248, 1e-15);
+        assert_almost_eq!(super::integral(2.5, 1).unwrap(), 0.0249149178702697399, 1e-15);
+        assert_almost_eq!(super::integral(10.0, 1).unwrap(), 4.15696892968532464e-06, 1e-20);
+        assert_eq!(super::integral(0.001, 2).unwrap(), 0.992668960469238915);
+        assert_almost_eq!(super::integral(0.1, 2).unwrap(), 0.722545022194020392, 1e-15);
+        assert_almost_eq!(super::integral(1.0, 2).unwrap(), 0.148495506775922048, 1e-16);
+        assert_almost_eq!(super::integral(2.0, 2).unwrap(), 0.0375342618204904527, 1e-16);
+        assert_almost_eq!(super::integral(10.0, 2).unwrap(), 3.830240465631608e-06, 1e-20);
+        assert_eq!(super::integral(0.001, 0).unwrap(), 999.000499833375);
+        assert_eq!(super::integral(0.1, 0).unwrap(), 9.048374180359595);
+        assert_almost_eq!(super::integral(1.0, 0).unwrap(), 0.3678794411714423, 1e-16);
+        assert_eq!(super::integral(2.0, 0).unwrap(), 0.06766764161830635);
+        assert_eq!(super::integral(10.0, 0).unwrap(), 4.539992976248485e-06);
     }
 }
