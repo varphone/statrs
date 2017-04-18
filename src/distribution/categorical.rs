9cc115a9339ad0d06aec49c4b9cdb8630d538be9
Michael Ma <michael60612@gmail.com>
2017-04-17 20:30:39 -0400

    feat: impl cdf for categorical

--- src/distribution/categorical.rs
+++ src/distribution/categorical.rs
@@ -2,7 +2,7 @@ use std::f64;
 use rand::Rng;
 use rand::distributions::{Sample, IndependentSample};
 use statistics::*;
-use distribution::{Distribution};
+use distribution::{Univariate, Discrete, Distribution};
 use {Result, StatsError};
 
 /// Implements the [Categorical](https://en.wikipedia.org/wiki/Categorical_distribution)
@@ -15,16 +15,13 @@ use {Result, StatsError};
 /// use statrs::statistics::Mode;
 /// ```
 #[derive(Debug, Clone, PartialEq)]
-pub struct Categorical
-{
+pub struct Categorical {
     norm_pmf: Vec<f64>,
     cdf: Vec<f64>,
 }
 
-impl Categorical
-{
-    pub fn new(prob_mass: &[f64]) -> Result<Categorical>
-    {
+impl Categorical {
+    pub fn new(prob_mass: &[f64]) -> Result<Categorical> {
         if !is_valid_prob_mass(prob_mass) {
             Err(StatsError::BadParams)
         } else {
@@ -54,10 +51,13 @@ impl Categorical
             })
         }
     }
+
+    fn cdf_max(&self) -> f64 {
+        *unsafe { self.cdf.get_unchecked(self.cdf.len() - 1) }
+    }
 }
 
-impl Sample<f64> for Categorical
-{
+impl Sample<f64> for Categorical {
     /// Generate a random sample from a categorical
     /// distribution using `r` as the source of randomness.
     /// Refer [here](#method.sample-1) for implementation details
@@ -66,8 +66,7 @@ impl Sample<f64> for Categorical
     }
 }
 
-impl IndependentSample<f64> for Categorical
-{
+impl IndependentSample<f64> for Categorical {
     /// Generate a random independent sample from a categorical
     /// distribution using `r` as the source of randomness.
     /// Refer [here](#method.sample-1) for implementation details
@@ -76,8 +75,7 @@ impl IndependentSample<f64> for Categorical
     }
 }
 
-impl Distribution<f64> for Categorical
-{
+impl Distribution<f64> for Categorical {
     /// Generate a random sample from the categorical distribution
     /// using `r` as the source of randomness
     ///
@@ -96,7 +94,7 @@ impl Distribution<f64> for Categorical
     /// # }
     /// ```
     fn sample<R: Rng>(&self, r: &mut R) -> f64 {
-        let draw = r.next_f64() * unsafe { self.cdf.get_unchecked(self.cdf.len() - 1) };
+        let draw = r.next_f64() * self.cdf_max();
         let mut idx = 0;
 
         if draw == 0.0 {
@@ -115,6 +113,64 @@ impl Distribution<f64> for Categorical
             el = unsafe { self.cdf.get_unchecked(idx) };
         }
         return idx as f64;
+    }
+}
+
+impl Univariate<u64, f64> for Categorical {
+    /// Calculates the cumulative distribution function for the categorical
+    /// distribution at `x`
+    ///
+    /// # Panics
+    ///
+    /// If `x < 0.0` or `x > k` where `k` is the number of categories
+    /// (i.e. the length of the `prob_mass` slice passed to the constructor)
+    ///
+    /// # Formula
+    ///
+    /// ```ignore
+    /// sum(p_j) from 0..x
+    /// ```
+    ///
+    /// where `p_j` is the probability mass for the `j`th category
+    fn cdf(&self, x: f64) -> f64 {
+        assert!(x >= 0.0 && x <= self.cdf.len() as f64,
+                format!("{}",
+                        StatsError::ArgIntervalIncl("x", 0.0, self.cdf.len() as f64)));
+        if x == self.cdf.len() as f64 {
+            1.0
+        } else {
+            unsafe { self.cdf.get_unchecked(x as usize) / self.cdf_max() }
+        }
+    }
+}
+
+impl Min<u64> for Categorical {
+    /// Returns the minimum value in the domain of the
+    /// categorical distribution representable by a 64-bit
+    /// integer
+    ///
+    /// # Formula
+    ///
+    /// ```ignore
+    /// 0
+    /// ```
+    fn min(&self) -> u64 {
+        0
+    }
+}
+
+impl Max<u64> for Categorical {
+    /// Returns the maximum value in the domain of the
+    /// categorical distribution representable by a 64-bit
+    /// integer
+    ///
+    /// # Formula
+    ///
+    /// ```ignore
+    /// n
+    /// ```
+    fn max(&self) -> u64 {
+        self.cdf.len() as u64 - 1
     }
 }
 
@@ -141,7 +197,9 @@ fn test_is_valid_prob_mass() {
 #[cfg_attr(rustfmt, rustfmt_skip)]
 #[cfg(test)]
 mod test {
-    use distribution::Categorical;
+    use std::fmt::Debug;
+    use statistics::*;
+    use distribution::{Univariate, Categorical};
 
     fn try_create(prob_mass: &[f64]) -> Categorical {
         let n = Categorical::new(prob_mass);
@@ -158,6 +216,15 @@ mod test {
         assert!(n.is_err());
     }
 
+    fn test_case<T, F>(prob_mass: &[f64], expected: T, eval: F)
+        where T: PartialEq + Debug,
+              F: Fn(Categorical) -> T
+    {
+        let n = try_create(prob_mass);
+        let x = eval(n);
+        assert_eq!(expected, x);
+    }
+
     #[test]
     fn test_create() {
         create_case(&[1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]);
@@ -167,5 +234,32 @@ mod test {
     fn test_bad_create() {
         bad_create_case(&[-1.0, 1.0]);
         bad_create_case(&[0.0, 0.0]);
+    }
+
+    #[test]
+    fn test_min_max() {
+        test_case(&[4.0, 2.5, 2.5, 1.0], 0, |x| x.min());
+        test_case(&[4.0, 2.5, 2.5, 1.0], 3, |x| x.max());
+    }
+
+    #[test]
+    fn test_cdf() {
+        test_case(&[0.0, 3.0, 1.0, 1.0], 3.0 / 5.0, |x| x.cdf(1.5));
+        test_case(&[1.0, 1.0, 1.0, 1.0], 0.25, |x| x.cdf(0.0));
+        test_case(&[4.0, 2.5, 2.5, 1.0], 0.4, |x| x.cdf(0.8));
+        test_case(&[4.0, 2.5, 2.5, 1.0], 1.0, |x| x.cdf(3.2));
+        test_case(&[4.0, 2.5, 2.5, 1.0], 1.0, |x| x.cdf(4.0));
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_cdf_input_low() {
+        test_case(&[4.0, 2.5, 2.5, 1.0], 1.0, |x| x.cdf(-1.0));
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_cdf_input_high() {
+        test_case(&[4.0, 2.5, 2.5, 1.0], 1.0, |x| x.cdf(4.5));
     }
 }
