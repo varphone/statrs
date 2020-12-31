a72054ef9195f77c9658cb55be6e905aefa5a0b9
James Millard <millard.jn@gmail.com>
2020-12-31 13:28:25 +1000

    test: confirm chi mean fails at dof > ~340
    This test shows that the mean of the chi distribution produces NaN at above approximately 340.0 degrees of freedom.
    When the dof exceeds this, the gamma function is called with an argument greater than approximately 170.0 producing a result with exceeds the maximum normal value for a f64.
    The equivalent limit for an f32 is approx half at a chi dof of 170.0 and a gamma argument of 85.0.
    As this method is key to distinguishing between stationary/nonstationary gaussian process, which can be high dimensional in many applications, this should be fixed.

--- src/distribution/chi.rs
+++ src/distribution/chi.rs
@@ -362,6 +362,14 @@ mod tests {
     }
 
     #[test]
+    fn test_large_dof_mean_not_nan() {
+        for i in 1..1000 {
+            let mean = Chi::new(i as f64).unwrap().mean().unwrap();
+            assert!(!mean.is_nan(), "Chi mean for {} dof was {}", i, mean);
+        }
+    }
+
+    #[test]
     #[should_panic]
     fn test_mean_degen() {
         let mean = |x: Chi| x.mean().unwrap();
