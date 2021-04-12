1b9f9ff7be747498ab1619856f5c5eb47b13474f
doraneko94 <shuntaro94@gmail.com>
2021-04-12 13:23:29 +0900

    fix typo in sample_unchecked() of gamma

--- src/distribution/gamma.rs
+++ src/distribution/gamma.rs
@@ -316,7 +316,7 @@ pub fn sample_unchecked<R: Rng + ?Sized>(rng: &mut R, shape: f64, rate: f64) -> 
         v *= v * v;
         x *= x;
         let u: f64 = rng.gen();
-        if u < 1.0 - 0.0331 * x * x || u.ln() < 0.5 * x + d * (1.0 - v - v.ln()) {
+        if u < 1.0 - 0.0331 * x * x || u.ln() < 0.5 * x + d * (1.0 - v + v.ln()) {
             return afix * d * v / rate;
         }
     }
