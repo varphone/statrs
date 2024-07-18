f57aab529ee0143d62e66063c2439257db8f635b
Orion Yeung <11580988+orionyeung001@users.noreply.github.com>
2024-07-18 12:03:59 -0500

    doc(fisher test): basic docs

--- src/stats_tests/fisher.rs
+++ src/stats_tests/fisher.rs
@@ -99,7 +99,8 @@ fn binary_search(
 
 /// Perform a Fisher exact test on a 2x2 contingency table.
 /// Based on scipy's fisher test: https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.fisher_exact.html#scipy-stats-fisher-exact
-/// Returns the odds ratio and p_value
+/// Expects a table in row-major order
+/// Returns the [odds ratio](https://en.wikipedia.org/wiki/Odds_ratio) and p_value
 /// # Examples
 ///
 /// ```
@@ -132,6 +133,7 @@ pub fn fishers_exact_with_odds_ratio(
 
 /// Perform a Fisher exact test on a 2x2 contingency table.
 /// Based on scipy's fisher test: https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.fisher_exact.html#scipy-stats-fisher-exact
+/// Expects a table in row-major order
 /// Returns only the p_value
 /// # Examples
 ///
--- src/stats_tests/mod.rs
+++ src/stats_tests/mod.rs
@@ -1,9 +1,16 @@
 pub mod fisher;
 
+/// Specifies an [alternative hypothesis](https://en.wikipedia.org/wiki/Alternative_hypothesis)
 #[derive(Debug, Copy, Clone)]
 pub enum Alternative {
+    #[doc(alias = "two-tailed")]
+    #[doc(alias = "two tailed")]
     TwoSided,
+    #[doc(alias = "one-tailed")]
+    #[doc(alias = "one tailed")]
     Less,
+    #[doc(alias = "one-tailed")]
+    #[doc(alias = "one tailed")]
     Greater,
 }
 
