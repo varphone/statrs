b79650754d4319b76baf8a9dd6284ce860c78c9d
Soumya <soumyasen1809@gmail.com>
2024-09-10 20:21:30 +0200

    Implemented Continuous CDF trait for Gumbel
    - Added cdf, inverse_cdf and sf for Gumbel
    - Added Min and Max traits for Gumbel
    - Added documentation for the same

--- src/distribution/gumbel.rs
+++ src/distribution/gumbel.rs
@@ -1,3 +1,9 @@
+use core::f64;
+
+use crate::statistics::{Max, Min};
+
+use super::ContinuousCDF;
+
 /// https://en.wikipedia.org/wiki/Gumbel_distribution
 #[derive(Copy, Clone, PartialEq, Debug)]
 pub struct Gumbel {
@@ -51,6 +57,85 @@ impl Gumbel {
 
 impl std::fmt::Display for Gumbel {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Gumbel({:?}, {:?})", self.location(), self.scale())
+        write!(f, "Gumbel({:?}, {:?})", self.location, self.scale)
+    }
+}
+
+impl ::rand::distributions::Distribution<f64> for Gumbel {
+    fn sample<R: rand::Rng + ?Sized>(&self, r: &mut R) -> f64 {
+        // Check: Quantile formula: mu - beta*ln(-ln(p))
+        self.location - self.scale * (((-(r.gen::<f64>())).ln()).ln())
+    }
+}
+
+impl ContinuousCDF<f64, f64> for Gumbel {
+    /// Calculates the cumulative distribution function for the
+    /// gumbel distribution at `x`
+    ///
+    /// # Formula
+    ///
+    /// ```text
+    /// e^(-e^(-(x - μ) / β))
+    /// ```
+    ///
+    /// where `μ` is the location and `β` is the scale
+    fn cdf(&self, x: f64) -> f64 {
+        (-(-(x - self.location) / self.scale).exp()).exp()
+    }
+
+    /// Calculates the inverse cumulative distribution function for the
+    /// gumbel distribution at `x`
+    ///
+    /// # Formula
+    ///
+    /// ```text
+    /// μ - β ln(-ln(p))
+    /// ```
+    ///
+    /// where `μ` is the location and `β` is the scale
+    fn inverse_cdf(&self, p: f64) -> f64 {
+        self.location - self.scale * ((-(p.ln())).ln())
+    }
+
+    /// Calculates the survival function for the
+    /// gumbel distribution at `x`
+    ///
+    /// # Formula
+    ///
+    /// ```text
+    /// 1 - e^(-e^(-(x - μ) / β))
+    /// ```
+    ///
+    /// where `μ` is the location and `β` is the scale
+    fn sf(&self, x: f64) -> f64 {
+        1.0 - (-(-(x - self.location) / self.scale).exp()).exp()
+    }
+}
+
+impl Min<f64> for Gumbel {
+    /// Returns the minimum value in the domain of the gumbel
+    /// distribution representable by a double precision float
+    ///
+    /// # Formula
+    ///
+    /// ```text
+    /// NEG_INF
+    /// ```
+    fn min(&self) -> f64 {
+        f64::NEG_INFINITY
+    }
+}
+
+impl Max<f64> for Gumbel {
+    /// Returns the maximum value in the domain of the gumbel
+    /// distribution representable by a double precision float
+    ///
+    /// # Formula
+    ///
+    /// ```text
+    /// f64::INFINITY
+    /// ```
+    fn max(&self) -> f64 {
+        f64::INFINITY
     }
 }
