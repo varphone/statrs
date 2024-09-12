a395dd12e0054fba860f54b0625babc9c730a838
Soumya <soumyasen1809@gmail.com>
2024-09-13 00:38:38 +0200

    feat: Add support for Gumbel distribution
    - Fix added: rand feature added to distribution
    to fix CI error

--- src/distribution/gumbel.rs
+++ src/distribution/gumbel.rs
@@ -112,6 +112,7 @@ impl std::fmt::Display for Gumbel {
     }
 }
 
+#[cfg(feature = "rand")]
 impl ::rand::distributions::Distribution<f64> for Gumbel {
     fn sample<R: rand::Rng + ?Sized>(&self, r: &mut R) -> f64 {
         self.location - self.scale * ((-(r.gen::<f64>())).ln()).ln()
